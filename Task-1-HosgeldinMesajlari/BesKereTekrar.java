a357b88ec0bf0c5ff6caca0dcf22f6dd770be70f
ArdaYesil <250542013@firat.edu.tr>
2025-10-21 20:55:42 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,20 +1,10 @@
 class Main {
     public static void main(String[] args) {
-
-        System.out.println("Java'ya Hoş Geldiniz");
-        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
-        System.out.println("Programlama Eğlencelidir");
-
         System.out.println("Java'ya Hoş Geldiniz");
         System.out.println("Java'ya Hoş Geldiniz");
         System.out.println("Java'ya Hoş Geldiniz");
         System.out.println("Java'ya Hoş Geldiniz");
         System.out.println("Java'ya Hoş Geldiniz");
-
-        System.out.println("    J A V V A");
-            System.out.println("  J A A V V A A");
-            System.out.println("J J AAAAA V V AAAAA");
-            System.out.println("  J J A A V A A");
-            
     }
 }
+
