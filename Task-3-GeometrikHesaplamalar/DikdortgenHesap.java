0e5e1efc79b1c770aaa190b1454f7facf082b92d
ArdaYesil <250542013@firat.edu.tr>
2025-10-21 21:13:05 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+ double KısaKenar=4.5;
+ double UzunKenar = 7.9;
+ double alan = KısaKenar*UzunKenar;
+ double cevre = 2 *(KısaKenar+UzunKenar) ;
+System.out.println("Dikdörtgenin Alanı :"+alan);
+System.out.println("Dikdörtgenin Çevresi :"+cevre);
+    }
+}
