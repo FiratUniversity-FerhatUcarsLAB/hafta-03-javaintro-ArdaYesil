f7af305728b279bb210235245cb77f5e1e982d67
ArdaYesil <250542013@firat.edu.tr>
2025-10-21 21:03:20 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+            double pay = 9.5*4.50-2.5*3;
+            double payda= 45.5-3.5;
+            double sonuc=pay/payda;
+            System.out.println(sonuc);
+            System.out.printf(pay+" / "+payda+" = "+"%.4f",sonuc);
+    }
+}
