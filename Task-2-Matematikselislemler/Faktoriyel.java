58c67fae8894c07a26afa1b11f2bba1712d10404
ArdaYesil <250542013@firat.edu.tr>
2025-10-21 21:02:27 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+            int f=5*4*3*2*1;
+            System.out.println("5! degeri ="+f);
+            System.out.println(f);
+    }
+}
