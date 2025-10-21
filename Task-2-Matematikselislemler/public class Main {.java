e40d10829d0e3757a383622119afb03fa4b62730
ArdaYesil <250542013@firat.edu.tr>
2025-10-21 21:05:36 +0300

    Add files via upload

---
+++ Task-2-Matematikselislemler/public class Main {.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+             int toplam=1+2+3+4+5+6+7+8+9;
+            System.out.println(toplam);
+            System.out.println("1+2+3+4+5+6+7+8+9="+toplam);
+            
+            double pay = 9.5*4.50-2.5*3;
+            double payda= 45.5-3.5;
+            double sonuc=pay/payda;
+            System.out.println(sonuc);
+            System.out.printf(pay+" / "+payda+" = "+"%.4f",sonuc);
+            
+            int f=5*4*3*2*1;
+            System.out.println("5! degeri ="+f);
+            System.out.println(f);
+    }
+}
