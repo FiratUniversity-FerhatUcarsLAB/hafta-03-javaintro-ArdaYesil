dbf645e0a44249b307d1cd45f16b92a9ca589327
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 17:51:18 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("  AAA    rrrr    dddd    aaa  ");
+        System.out.println(" A   A  r   r   d   d  a   a ");
+        System.out.println(" AAAAA  rrr     d   d  aaaaa ");
+        System.out.println(" A   A  r  r    d   d  a   a ");
+        System.out.println(" A   A  r   r   dddd   a   a ");
+    }
+}
