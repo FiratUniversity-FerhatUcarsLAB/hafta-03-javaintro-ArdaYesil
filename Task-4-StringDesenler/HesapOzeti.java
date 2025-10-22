a6fb9b1dfda494ade32892d4d0ad764d00c0b29c
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 16:55:11 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Ürün     Miktar    Fiyat");
+        System.out.println(" ===================== ");
+        System.out.println("Ekmek      15      10TL");
+        System.out.println(" ===================== ");
+        System.out.println("Peynir     1kg     125TL");
+        System.out.println(" ===================== ");
+        System.out.println("Süt        2L      65TL");
+        System.out.println(" ===================== ");
+        System.out.println("Domates    2kg     60TL");
+        System.out.println(" ===================== ");
+        System.out.println("Zeytin     1kg     100TL");
+    }
+}
+
