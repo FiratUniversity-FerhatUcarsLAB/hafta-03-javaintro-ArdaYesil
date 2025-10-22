70195ad848332c342e78d0ccfeaff7946d6681cc
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 17:02:23 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/public class Main {.java
@@ -0,0 +1,44 @@
+class Main {
+    public static void main(String[] args) {
+
+        String ad = " Arda";
+        String soyad = " Yeşil";
+        String ogrNo = " 250542013";
+        String bolum = " Yazilim Muh.";
+
+        System.out.println(" =================== ");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println(" =================== ");
+        System.out.println("Ad    :" + ad);
+        System.out.println("Soyad :" + soyad);
+        System.out.println("No    :" + ogrNo);
+        System.out.println("Bolum :" + bolum);
+        System.out.println(" =================== ");
+
+        System.out.println("");
+
+        System.out.println("      /\\    ");
+        System.out.println("     /  \\   ");
+        System.out.println("    /----\\  ");
+        System.out.println("   /      \\ ");
+        System.out.println("  /        \\");
+
+        System.out.println("");
+
+        System.out.println("Ürün     Miktar    Fiyat");
+        System.out.println(" ===================== ");
+        System.out.println("Ekmek      15      10TL");
+        System.out.println(" ===================== ");
+        System.out.println("Peynir     1kg     125TL");
+        System.out.println(" ===================== ");
+        System.out.println("Süt        2L      65TL");
+        System.out.println(" ===================== ");
+        System.out.println("Domates    2kg     60TL");
+        System.out.println(" ===================== ");
+        System.out.println("Zeytin     1kg     100TL");
+
+
+        
+    }
+}
+
