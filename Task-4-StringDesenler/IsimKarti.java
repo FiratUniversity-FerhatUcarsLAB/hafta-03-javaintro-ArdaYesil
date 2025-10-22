3faeb7ceb7d14f8b1d201a9d532aee322aa8600c
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 16:08:23 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+
+        String ad = " Arda";
+        String soyad = " Yeşil";
+        String ogrNo = " 250542013";
+        String bolum = " Yazilim Muh.";
+
+        System.out.println(" =================== ");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println(" =================== ");
+        System.out.println("Ad    :" + ad);
+        System.out.println("Soyad :" + soyad);
+        System.out.println("No    :" + ogrNo);
+        System.out.println("Bolum :" + bolum);
+        System.out.println(" =================== ");
+    }
+}
