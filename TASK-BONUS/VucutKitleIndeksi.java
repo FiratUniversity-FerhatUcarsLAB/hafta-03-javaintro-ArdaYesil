9496f69b50bbfdcbbe20dbf28060a0285db33ab8
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 18:14:07 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        double BMI,Kilo=63,Boy=1.80;
+        BMI= Kilo / (Boy * Boy);
+        System.out.println("Vücut kütle indexiniz :"+BMI);
+    }
+}
