5785bdad1066322aeeecb0655042cbfc5b1d041a
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 15:59:35 +0300

    Update DaireHesap.java

--- Task-3-GeometrikHesaplamalar/DaireHesap.java
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -2,10 +2,11 @@ class Main {
     public static void main(String[] args) {
  double r = 5.5;
  double pi = 3.14159;
- double alan = pi * r * r ;
- double cevre = 2 * pi * r ;
+ double Dairealan = pi * r * r ;
+ double Dairecevre = 2 * pi * r ;
  System . out . println (" Yaricap : " + r ) ;
- System . out . println (" Alan : " + alan ) ;
- System . out . println (" Cevre : " + cevre ) ;
+ System . out . println (" Alan : " + Dairealan ) ;
+ System . out . println (" Cevre : " + Dairecevre ) ;
     }
 }
+
