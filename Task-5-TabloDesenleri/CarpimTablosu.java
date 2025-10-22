5681df4b264c4518e8980004bfa97e04f0218c1d
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 17:29:17 +0300

    Update CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+            System.out.println("a   a^2   a^3   a^4   a^5");
+            System.out.println("--------------------------");
+            System.out.println("1    1     1     1     1 ");
+            System.out.println("2    4     8     16    32");
+            System.out.println("3    9     27    81    243");
+            System.out.println("4    16    64    256   1024");
+            System.out.println("5    25    125   625   3125");
+    }
+}
+
