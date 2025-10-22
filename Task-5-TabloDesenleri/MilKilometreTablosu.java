8e11e9a518807aa5226cffc5cd95d0364aecc08b
ArdaYesil <250542013@firat.edu.tr>
2025-10-22 17:34:43 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+            System.out.println(" Mil ------->  Kilometre");
+            System.out.println("-----------------------");
+            System.out.println(" 01             1.609");
+            System.out.println(" 05             8.045");
+            System.out.println(" 10             16.090");
+            System.out.println(" 20             32.180");
+            System.out.println(" 50             80.450");
+    }
+}
+
